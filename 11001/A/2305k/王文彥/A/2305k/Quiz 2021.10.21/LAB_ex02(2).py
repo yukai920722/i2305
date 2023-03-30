64bca2e4c3c77f23d6d4ef54fa379625f51672c8
王榆凱 <90737336+yukai920722@users.noreply.github.com>
2023-03-30 10:20:49 +0800

    Add files via upload

---
+++ 11001/A/2305k/王文彥/A/2305k/Quiz 2021.10.21/LAB_ex02(2).py
@@ -0,0 +1,9 @@
+# Circle area
+def findArea(r):
+    PI = 3.142
+    return PI * (r*r)
+ 
+# 
+print("圆的面積為 %.2f" % findArea(5))
+
+
---
+++ 11001/A/2305k/王文彥/A/homework3.py
@@ -0,0 +1,27 @@
+def function(a = "ok"):
+    print(a)
+
+function("helloeorld")
+function("hi")
+function("")
+function()
+
+def function(a,b):
+    print(a+b)
+
+function(2,4)
+function(2,5)
+function(4,8)
+function(8,6)
+function(9,9)
+
+def function(number = 20):
+    print(10 + number)
+
+function(5)
+function()
+function(10)
+
+function = lambda x,y,z:x+y+z
+print(function(2,4,5))
+
